c2e87d320e21da6e2b632faff9ace9be6cd0cf79
chenyunguiMilook <linxitj2003@icloud.com>
2025-03-05 09:50:05 +0800

    remove unused code

---
+++ Sources/VisualDebugger/render/base/static/StaticRendable.swift
@@ -0,0 +1,24 @@
+//
+//  Rotateable.swift
+//  VisualDebugger
+//
+//  Created by chenyungui on 2025/3/2.
+//
+
+import CoreGraphics
+
+public protocol Cloneable {
+    func clone() -> Self
+}
+
+public protocol StaticRendable: Cloneable {
+    // raw bounds
+    var contentBounds: CGRect { get }
+    
+    func render(
+        with transform: Matrix2D,
+        in context: CGContext,
+        scale: CGFloat,
+        contextHeight: Int?
+    )
+}
